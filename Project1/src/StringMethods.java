337dae77ee924a5dd9f95cac7284798c3ee9e348
PC <PC@Dell>
2017-10-05 18:31:32 -0400

    Project1

--- Project1/src/StringMethods.java
+++ Project1/src/StringMethods.java
@@ -1,4 +1,12 @@
 
 public class StringMethods {
-
+	
+	public String dateStr 
+	
 }
+
+	public String cutOff(String mainStr, String subStr) {
+		 String first = str.substring(mainStr.length()*1/2); 
+		 String last = str.substring(subStr.length()*1/2); 
+		 return(mainStr + subStr);
+	}
