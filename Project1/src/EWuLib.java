66ad6c8b74f55be6866a06485d68416b92d9294f
ewu9995 <32362354+ewu9995@users.noreply.github.com>
2017-10-06 09:19:29 -0400

    Update EWuLib.java

--- Project1/src/EWuLib.java
+++ Project1/src/EWuLib.java
@@ -5,7 +5,7 @@ public class EwuLib {
 	
 	}
 
-	public String cutOff(String mainStr, String subStr) {
+	public String cutOut(String mainStr, String subStr) {
 		 String first = mainStr.substring(0, mainStr.length()*1/2); 
 		 String last = subStr.substring(subStr.length()*1/2); 
 		 return(first + last);
