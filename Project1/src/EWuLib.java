e50047accdd6e3c91f492adf232a6d7856fd8b6c
ewu9995 <32362354+ewu9995@users.noreply.github.com>
2017-10-05 23:12:14 -0400

    Update EWuLib.java

--- Project1/src/EWuLib.java
+++ Project1/src/EWuLib.java
@@ -2,10 +2,14 @@ public class EwuLib {
 	
 	public String dateStr 
 	
-}
+	}
 
 	public String cutOff(String mainStr, String subStr) {
 		 String first = mainStr.substring(0, mainStr.length()*1/2); 
 		 String last = subStr.substring(subStr.length()*1/2); 
 		 return(first + last);
 	}
+
+	public static void isFibonnaci() {
+		for (int x =1; x ; x++) {
+		if (x++ == (	
