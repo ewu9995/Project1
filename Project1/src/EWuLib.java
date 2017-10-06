af8a62d605cac583078b5f3451a15ace6e6d5adc
ewu9995 <32362354+ewu9995@users.noreply.github.com>
2017-10-05 23:06:48 -0400

    Create EWuLib.java

---
+++ Project1/src/EWuLib.java
@@ -0,0 +1,11 @@
+public class EwuLib {
+	
+	public String dateStr 
+	
+}
+
+	public String cutOff(String mainStr, String subStr) {
+		 String first = mainStr.substring(0, mainStr.length()*1/2); 
+		 String last = subStr.substring(subStr.length()*1/2); 
+		 return(first + last);
+	}
