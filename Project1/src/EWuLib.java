9cfe558786debf7601744ad2733a378cd4f8286f
ewu9995 <32362354+ewu9995@users.noreply.github.com>
2017-10-07 23:40:13 -0400

    Update EWuLib.java

--- Project1/src/EWuLib.java
+++ Project1/src/EWuLib.java
@@ -9,8 +9,11 @@ public class EWuLib {
 	}
 
 	public String cutOut(String mainStr, String subStr) {
+		 //first is the first half of the output
 		 String first = mainStr.substring(0, mainStr.length()*1/2); 
+		 //last is the last half of the output
 		 String last = subStr.substring(subStr.length()*1/2); 
+		 // below will return the two halves
 		 return(first + last);
 	}
 
