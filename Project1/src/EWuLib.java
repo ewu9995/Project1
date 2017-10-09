b1e9367cdb983a627e73e6b686713405e7d89cbd
ewu9995 <32362354+ewu9995@users.noreply.github.com>
2017-10-09 10:08:47 -0400

    Update EWuLib.java

--- Project1/src/EWuLib.java
+++ Project1/src/EWuLib.java
@@ -1,26 +1,24 @@
 // Evan Wu Project1
 public class EWuLib {
-	public static void main(String[] args) {
-	}
+
 	
-	public static String dateStr(String mm, String dd, String yyyy) {
-		return (dd + "-" + mm +"-" + yyyy);
-		
-		
-		
+	public static void dateStr(String mm, String dd, String yyyy) {
+		System.out.println(dd + "-" + mm +"-" + yyyy);
 	}
 
-	public static String cutOut(String mainStr, String subStr) {
+	
+	
+	public static void cutOut(String mainStr, String subStr) {
 		 //first is the first half of the output
 		 String first = mainStr.substring(0, mainStr.length()*1/2); 
 		 //last is the last half of the output
 		 String last = subStr.substring(subStr.length()*1/2); 
 		 // below will return the two halves
-		 return(first + last);
+		 System.out.println(first + last);
 	}
 
 	
-		public static boolean isFinnobaci(int num) {
+	public static boolean isFinnobaci(int num) {
 				int x = 0;
 				while (x<= num)
 				{
@@ -35,6 +33,11 @@ public class EWuLib {
 				return false;
 					}
 
+		
+	
+
 	}
 		
+
+
 
