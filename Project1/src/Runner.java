c98deea54bade1ca6009ccc4864fc10195c205ed
ewu9995 <32362354+ewu9995@users.noreply.github.com>
2017-11-01 19:57:18 -0400

    Updated runner to see if method cutOut works
    fixed method cutOut -- please recheck

--- Project1/src/Runner.java
+++ Project1/src/Runner.java
@@ -1,11 +1,12 @@
-//Evan Wu and Jeffery Lau 
+//Evan Wu and Jeffrey Lau
 //Project 1
 public class Runner {
 	public static void main(String [] args) {
-		EWuLib.dateStr(); // Enter input in form of mm/dd/yyyy
-		EWuLib.cutOut(); // Enter input of two words/strings
-		EWuLib.isFibonacci(); // Enter any integer
-		
+		EWuLib.dateStr("11-01-2017"); // Enter input in form of mm/dd/yyyy
+		EWuLib.cutOut("ABCD","BC"); // Enter input of two words/strings
+		EWuLib.isFibonacci(5); // Enter any integer
+
+		//returns error???
 		JLauLib.sumUpTo(); // Enter any integer
 		JLauLib.multiplicationtable(); // Enter a base and range
 		JLauLib.isPalindrome(); // Enter any string/word
